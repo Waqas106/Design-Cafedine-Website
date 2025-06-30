f59a704132d433978f35f04bb81b7337de508096
Waqas106 <m.waqas987321@gmail.com>
2025-06-30 17:13:41 +0500

    Delete Design Cafedine Website directory

--- Design Cafedine Website/main.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById("mobile-toggle");
-const navlinks = document.getElementById("navlinks");
-
-toggleBtn.addEventListener("click", function() {
-    navlinks.classList.toggle("show");
-});
